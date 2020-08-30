0cbcb66ed7f15a599125a66f05f670f0c37e8638
adityarawat007 <69760948+adityarawat007@users.noreply.github.com>
2020-08-30 19:35:28 +0530

    Add files via upload

---
+++ armstrong.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,r,sum=0,m;
+
+    cout<<"enter no"<<endl;
+    cin>>n;
+    m=n;
+
+    while(n>0)
+    {
+        r=n%10;
+        n=n/10;
+        sum=sum+r*r*r;
+    }
+    if(sum==m)
+    {
+        cout<<"Armstrong number";
+    }
+    else
+    {
+        cout<<"Not Armstrong number";
+    }
+
+    return 0;
+}
